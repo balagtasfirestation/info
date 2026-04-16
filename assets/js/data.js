59e80b72e0d9b8c7565b9fddc5b2145d2c2e1cbf
unknown <firestationbalagtas312@gmail.com>
2026-04-16 14:44:24 +0800

    enhancements april 16

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,16 +2,19 @@ const herodata = [
   { 
     title: "Balagtas Fire Station", 
     description: "The BFP R3 Balagtas Fire Station is located in Borol 1st, Balagtas, 3016 Bulacan, Philippines. It operates 24/7 and can be reached for emergencies at 09257348169 or (044) 462 6177, or via 911. It serves as the primary fire safety unit for the municipality. Key Information for Balagtas Fire Station: Social Media: BFP R3 Balagtas Fire Station on Facebook, Operating Hours: Always open. The station handles fire suppression, prevention, and related emergencies within the municipality of Balagtas.", 
-    img: "Balagtas-fs-building.jpg" 
+    img: "Balagtas-fs-building.jpg", 
+    video: "https://www.youtube.com/watch?v=mmgQdso1WZI" 
   },
   { 
     title: "Mission", 
     description: "We protect life, property, and the environment through an integrated system of prevention, response, and investigation, strengthening public safety and community resilience", 
-    img: "Mission.jpg" 
+    img: "Mission.jpg", 
+    video: "https://www.youtube.com/watch?v=s0cuvBDhEE0" 
   },
   { 
     title: "Vision", 
     description: "A modern and trusted fire and emergency service building safe and resilient communities", 
-    img: "Vision.jpg" 
+    img: "Vision.jpg", 
+    video: "https://www.youtube.com/watch?v=s0cuvBDhEE0" 
   }
 ];
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,13 +5,13 @@ herodata.forEach(item => {
         <div class="item">
             <div class="main-slider-one__item">
                 <div class="main-slider-one__bg" style="background-image: url(assets/images/background/${item.img});"></div>
-                <div class="container">
+                <div class="container my-3">
                     <div class="row">
                         <div class="col-md-12">
                             <div class="main-slider-one__content">
                                 <h2 class="main-slider-one__title fire-text">${item.title}</h2>
                                 <p class="main-slider-one__text">${item.description}</p>
-                                <div class="main-slider-one__btn"> <a href="services.html" class="firdip-btn main-slider-one__btn__link">Discover More</a></div>
+                                <div class="main-slider-one__btn"> <a href="${item.video}" target="_blank" class="firdip-btn main-slider-one__btn__link">Discover More</a></div>
                             </div>
                         </div>
                     </div>
