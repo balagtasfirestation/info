ffc518e8215db99ef71390e8061345e5e07ff6a3
unknown <firestationbalagtas312@gmail.com>
2026-05-02 15:05:44 +0800

    changed the image carousel hero into video background

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,27 +1,29 @@
-let heroprint = '';
+// let heroprint = '';
 
-herodata.forEach(item => {
-    heroprint += `
-        <div class="item">
-            <div class="main-slider-one__item">
-                <div class="main-slider-one__bg" style="background-image: url(assets/images/background/${item.img});"></div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="main-slider-one__content">
-                                <h2 class="main-slider-one__title fire-text">${item.title}</h2>
-                                <p class="main-slider-one__text">${item.description}</p>
-                                <div class="main-slider-one__btn"> <a href="${item.video}" target="_blank" class="firdip-btn main-slider-one__btn__link">Discover More</a></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `;
-});
+// herodata.forEach(item => {
+//     heroprint += `
+//         <div class="item">
+//             <div class="main-slider-one__item">
+//                 <div class="main-slider-one__bg" style="background-image: url(assets/images/background/${item.img});"></div>
+//                 <div class="container">
+//                     <div class="row">
+//                         <div class="col-md-12">
+//                             <div class="main-slider-one__content">
+//                                 <h2 class="main-slider-one__title fire-text">${item.title}</h2>
+//                                 <p class="main-slider-one__text">${item.description}</p>
+//                                 <div class="main-slider-one__btn"> 
+//                                     <a href="${item.video}" target="_blank" class="firdip-btn main-slider-one__btn__link">Discover More</a>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     `;
+// });
 
-document.getElementById('mainSlider_print').innerHTML = heroprint;
+// document.getElementById('mainSlider_print').innerHTML = heroprint;
 
 let newsPrint = '';
 
